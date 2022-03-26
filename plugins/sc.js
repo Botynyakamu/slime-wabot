173080cdafab8e18f15163462cc1a1f1bd8c944f
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 08:58:22 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -3,16 +3,16 @@ let fetch = require('node-fetch')
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
 let info = `╭═[ *SCRIPT* ]═────···
 ┴
-│ *Script :* 60K (No node_module)
-│ *Base :*
-│ ╰ https://github.com/nurutomo/wabot-aq
+│ *Script :* 30K (No node_module)
+│ *no owner :*
+│ ╰ https://wa.me/6285828764046
 │ *RestApi :* 
 │ ╰ https://hardianto.xyz
 ┬
 ╰──────────═┅═──────────
 [❗] Jangan diperjual belikan tanpa seizin owner
 `.trim()
-await conn.send2Button(m.chat, info, `${botol}`, '⋮☰ Back', '.menu', 'Owner', '.nowner', m)
+await conn.send2Button(m.chat, info, `${botol}`, '⋮☰ Back', '.menu', 'Owner', '.owner', m)
 conn.reply(info)
 }
 handler.help = ['sc']
