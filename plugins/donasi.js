bafbf81c68138182724a725f1a841cc53c3e53db
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 08:16:12 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6282195470493
-│┝‷✧ *Dana:* 6282195470493
+│┝‷✧ *gopay:* 6285828765046
+│┝‷✧ *Dana:* 6285828764046
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6281354079196?text=kak+mau+donasi
+│wa.me/6285828764046?text=kak+mau+donasi
 ╰──────────···───╮
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Owner', '.owner')
