9d210a180d7636242f5c01362ef2f51b305630a6
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 08:55:24 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,12 +1,28 @@
+let { MessageType } = require('@adiwajshing/baileys')
+
 let handler = async (m, { conn, text, participants }) => {
+   r = text || 'gada pesannya'
+   l = conn.getName(m.sender)
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+  conn.reply(m.chat, '║╭──❉ 〔 TAGALL 〕\n│\n║│➸*Nama:* ' + l +  '\n║│➸ *Pesan:* ' + r +'\n│\n' + users.map(v => '║│➸@' + v.replace(/@.+/, '')).join`\n`, m, {
     contextInfo: { mentionedJid: users }
-  })
+ 
+  
+})
 }
-handler.command = ['tagall']
+handler.command = /^(tagall|everyone)$/i
+handler.tags = ["group"]
+handler.help = ["tagall", "everyone"].map(a => a + ' <pesan>')
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
 
 handler.admin = true
-handler.group = true
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
 
 module.exports = handler
