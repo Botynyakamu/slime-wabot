33e2d7fa5857617a3e71263c2ad211bf789308a9
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-26 08:06:19 +0700

    Update TQTO.js

--- plugins/TQTO.js
+++ plugins/TQTO.js
@@ -3,14 +3,14 @@
 //kalo mau ada nama kalian chat dulu ke ibnu,izin dulu biar sama² enak
 //di tambahin nama boleh tapi di ilagin jangan walau satu nama pun
 //yg gk tau nomer raku nih nomer nya
-//http://wa.me/6287834993722?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
+//http://wa.me/6285828764046?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
 *Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
 `.trim(), m)
 }
-handler.help = ['ɴᴜʀᴜᴛᴏᴍᴏ','ᴀᴅɪᴡᴀᴊsʜɪɴɢ','ʀᴇɴᴅʏᴄʀᴀғᴛ','ɪʙɴᴜ ɴʀ','ʙᴀᴋᴀ ʙᴏᴛᴢ', 'ᴀʀɪғғʙ25','ᴀᴛᴇɴᴀʙᴏᴛ','ʙᴀᴋᴀ ᴛᴇᴀᴍ','ᴀᴅʀɪ']
+handler.help = ['ɴᴜʀᴜᴛᴏᴍᴏ','ᴀᴅɪᴡᴀᴊsʜɪɴɢ','ʀᴇɴᴅʏᴄʀᴀғᴛ','ɪʙɴᴜ ɴʀ','ʙᴀᴋᴀ ʙᴏᴛᴢ','Rozi','ᴀʀɪғғʙ25','ᴀᴛᴇɴᴀʙᴏᴛ','ʙᴀᴋᴀ ᴛᴇᴀᴍ','ᴀᴅʀɪ']
 handler.tags = ['thnks']
 handler.customPrefix = /(\?$)/
 handler.command = /^kannakawai$/i
