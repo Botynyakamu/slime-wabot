0e6fe458841301bfbe4d22fc09f5f3f4efc40072
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-27 21:38:17 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -6,16 +6,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 
     "displayName": "OWNER",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;owner;;;\nFN:OWNER\nitem1.TEL;waid=6285828764046:6285828764046\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:roziganteng559@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Amrozi888\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ ZIFABOTZ ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:OWNER;;;\nFN:OWNER\nitem1.TEL;waid=6285828764046:6285828764046\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:roziganteng559@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Amrozi888\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ ZIFABOTZ ]───────\nEND:VCARD"
   }, {
-    "displayName": "Whatsapp",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ By WhatsApp ]───────\nEND:VCARD"
+    "displayName": "BOT UTAMA",
+      "vcard": ""BEGIN:VCARD\nVERSION:3.0\nN:bot utama;;;\nFN:BOT\nitem1.TEL;waid=6283865196475:62853865196475\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:roziganteng559@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Amrozi888\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ ZIFABOTZ ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
 handler.help = ['owner']
 handler.tags = ['info']
 
-handler.command = /^(owner)$/i
+handler.command = /^(botutama)$/i
 
 module.exports = handler
